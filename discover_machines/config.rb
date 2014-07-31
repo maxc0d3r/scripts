9ca9f26872a57ab41f97068b5e6400fb1a8797dd
maxc0d3r <mail2mayank@gmail.com>
2014-07-31 09:52:28 +0530

    publish the records to google spreadsheet called DNS

--- discover_machines/config.rb
+++ discover_machines/config.rb
@@ -3,3 +3,5 @@
 @aws_access_key_id = "<ADD_YOUR_IAM_ACCESS_KEY_HERE>"
 @aws_secret_access_key = "<ADD_YOUR_IAM_SECRET_KEY_HERE>"
 @aws_region = "<ADD_YOUR_AWS_REGION_HERE>"
+@username = "<YOUR_GOOGLE_ID>"
+@password = "<YOUR_GOOGLE_PASSWORD>"
--- discover_machines/discover.rb
+++ discover_machines/discover.rb
@@ -2,6 +2,7 @@
 
 require 'fog'
 require 'optparse'
+require 'google_drive'
 require_relative 'config.rb'
 
 $conn = Fog::Compute.new(
@@ -38,14 +39,60 @@ $filters=filter_criterias.split(',').inject(Hash.new{|h,k|h[k]=[]}) do |h,s|
 end
 
 
-def get_servers()
+def get_servers(environment)
   server_details = Array.new()
+  $filters['tag:Environment'] = ["#{environment}"]
   servers = $conn.servers.all($filters)
   ipaddress = Array.new
   servers.each do |server|
-    server_details  << "#{server.tags["Name"]}.#{server.tags["Environment"]}.indix.tv"
+    server_details  << { "name" => "#{server.tags["Name"]}.#{server.tags["Environment"]}.indix.tv", "cluster" => "#{server.tags["Cluster"]}" }
   end
-  puts server_details
+  server_details
 end
 
-get_servers
+def populate_spreadsheet
+  session = GoogleDrive.login(@username,@password)
+  spreadsheetName="DNS"
+  worksheetNames=["production","production-mr","staging","staging-mr","ft","uat","all"]
+  instance_variable_names=["production","production_mr","staging","staging_mr","ft","uat","all"]
+  spreadsheetHandle = session.spreadsheet_by_title("#{spreadsheetName.strip}")
+
+  if spreadsheetHandle.nil?
+    spreadsheetHandle = session.create_spreadsheet(title="#{spreadsheetName.strip}")
+    worksheetNames.each_with_index do |ws,index|
+      instance_variable_set("@#{instance_variable_names[index]}", spreadsheetHandle.add_worksheet(ws))
+    end
+  else
+    worksheetNames.each_with_index do |ws,index|
+      if spreadsheetHandle.worksheet_by_title(ws).nil?
+        instance_variable_set("@#{instance_variable_names[index]}",spreadsheetHandle.add_worksheet(ws))
+      else
+        spreadsheetHandle.worksheet_by_title(ws).delete
+        instance_variable_set("@#{instance_variable_names[index]}",spreadsheetHandle.add_worksheet(ws))
+      end
+    end
+  end
+
+  schema = ["DNS","Cluster"]
+  worksheetNames.each_with_index do |ws_name,index|
+    ws = instance_variable_get("@#{instance_variable_names[index]}")
+    i=1
+    schema.each do |column_name|
+      ws[1,i] = column_name
+      i=i+1
+    end
+  end
+
+  worksheetNames.each_with_index do |ws_name,index|
+    ws = instance_variable_get("@#{instance_variable_names[index]}")
+    i=2
+    get_servers(ws_name).each do |server_details|
+      ws[i,1] = server_details["name"]
+      ws[i,2] = server_details["cluster"]
+      i=i+1
+    end
+    ws.save
+  end
+end
+
+populate_spreadsheet
